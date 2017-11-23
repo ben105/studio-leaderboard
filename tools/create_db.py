089edb5d173e92d3cb1043317938c3c571f6ecbc
Ben Rooke <ben_rooke@icloud.com>
2017-11-23 13:33:33 -0800

    Initial commit with basic methods for using PerfectMIND API and placeholders for db creation.

---
+++ tools/create_db.py
@@ -0,0 +1,13 @@
+import studio
+import sqlite3
+
+def table_exists():
+  # Check if the table exists fitst. 
+  # If no records, bail successfully.
+  # Otherwise, if records exist, throw an error.
+
+def create_table():
+  # Attempt to create the table.
+
+def create_contacts():
+  # Use the studio library to create the contact list.
